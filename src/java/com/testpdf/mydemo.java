d8527f243edb5c1c2ac9b73e38203192db081478
Ing. Joe Ronald Florez Rada <joerfrada@gmail.com>
2022-01-02 21:33:59 -0500

    Tomcat Servlet with iText 5 PDF by Joe Ronald Florez Rada

---
+++ src/java/com/testpdf/mydemo.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.testpdf;
+
+import java.io.IOException;
+import java.util.*;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+
+/**
+ *
+ * @author admin
+ */
+public class mydemo extends HttpServlet {
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("application/pdf");
+        
+        try {
+            Document doc = new Document();
+            PdfWriter.getInstance(doc, response.getOutputStream());
+            
+            doc.open();
+            
+            doc.add(new Paragraph("Hola Mundo!"));
+            doc.add(new Paragraph(new Date().toString()));
+            
+            doc.close();
+        }
+        catch (DocumentException e) {
+            throw new IOException(e.getMessage());
+        }
+    }
+
+}
